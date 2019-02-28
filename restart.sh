7c114ee82e99b1f64e2b5d166bee1eb497f90b72
ComeBey <46746974+ComeBey@users.noreply.github.com>
2019-02-28 18:39:02 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -6,19 +6,19 @@
 #   Visit: https://www.youtube.com/channel/UCiMTLB5vELIq3Dz9muBRfJw?view_as=subscriber#
 #=====================================================================================#
 
-name=(ShadowsocksR ShadowsocksR-Python ShadowsocksR ShadowsocksR-Go ShadowsocksR-libev)
+name=(shadowsocksr shadowsocksr-Python ShadowsocksR ShadowsocksR-Go ShadowsocksR-libev)
 path=/var/log
 [[ ! -d ${path} ]] && mkdir -p ${path}
-log=${path}/ShadowsocksR-crond.log
+log=${path}/shadowsocksr-crond.log
 
-ShadowsocksR_init[0]=/etc/init.d/ShadowsocksR
-ShadowsocksR_init[1]=/etc/init.d/ShadowsocksR-python
-ShadowsocksR_init[2]=/etc/init.d/ShadowsocksR-r
-ShadowsocksR_init[3]=/etc/init.d/ShadowsocksR-go
-ShadowsocksR_init[4]=/etc/init.d/ShadowsocksR-libev
+shadowsocksr_init[0]=/etc/init.d/shadowsocksr
+shadowsocksr_init[1]=/etc/init.d/shadowsocksr-python
+shadowsocksr_init[2]=/etc/init.d/shadowsocksr-r
+shadowsocksr_init[3]=/etc/init.d/shadowsocksr-go
+shadowsocksr_init[4]=/etc/init.d/shadowsocksr-libev
 
 i=0
-for init in "${ShadowsocksR_init[@]}"; do
+for init in "${shadowsocksr_init[@]}"; do
     pid=""
     if [ -f ${init} ]; then
         ss_status=$(${init} status)
